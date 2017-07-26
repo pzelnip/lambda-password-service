03549f05e56d9d49783a88e83a81190146155273
Adam Parkin <pzelnip@gmail.com>
2017-07-26 14:11:48 -0700

    Cleaner Tests

--- test/index_test.py
+++ test/index_test.py
@@ -3,13 +3,13 @@ import unittest
 from index import lambda_handler
 
 
+SAMPLE_SHA256_HASH = '$pbkdf2-sha256$29000$.L93bg0BwFiLEaL0fm8NIQ$yYmxiSuP9pXXbrO4cT6CkE1QaNKpt8PjugrgvOBfcRY'
+SAMPLE_PASSWORD = 'password'
+
+
 class TestLambdaHandler(unittest.TestCase):
     def test_valid_sha256_hash_with_matching_password_returns_true(self):
-        event = {
-            "digest": "sha256",
-            "hash_pass": "$pbkdf2-sha256$29000$.L93bg0BwFiLEaL0fm8NIQ$yYmxiSuP9pXXbrO4cT6CkE1QaNKpt8PjugrgvOBfcRY",
-            "password": "password"
-        }
+        event = _build_event('sha256', SAMPLE_SHA256_HASH, SAMPLE_PASSWORD)
         expected = True
 
         result = lambda_handler(event, None)
@@ -17,13 +17,17 @@ class TestLambdaHandler(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_valid_sha256_hash_with_wrong_password_returns_false(self):
-        event = {
-            "digest": "sha256",
-            "hash_pass": "$pbkdf2-sha256$29000$.L93bg0BwFiLEaL0fm8NIQ$yYmxiSuP9pXXbrO4cT6CkE1QaNKpt8PjugrgvOBfcRY",
-            "password": "this is not the password"
-        }
+        event = _build_event('sha256', SAMPLE_SHA256_HASH, 'this is not the password')
         expected = False
 
         result = lambda_handler(event, None)
 
         self.assertEqual(expected, result)
+
+
+def _build_event(digest, hash_pass, password):
+    return {
+        "digest": digest,
+        "hash_pass": hash_pass,
+        "password": password,
+    }
