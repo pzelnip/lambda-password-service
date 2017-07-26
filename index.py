d3ba0ce251f5086d07a3e9bba6a487ce361543ec
Adam Parkin <pzelnip@gmail.com>
2017-07-26 09:19:49 -0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import httplib
+import urllib
+import json
+
+from passlib.hash import pbkdf2_sha256
+from passlib.hash import pbkdf2_sha512
+from passlib.hash import pbkdf2_sha1
+
+
+def lambda_handler(event, context):
+    digest = event['digest']
+    hash_pass = event['hash_pass']
+    password = event['password']
+
+    if digest == "sha256":
+        verification = pbkdf2_sha256.verify(password, hash_pass)
+    elif digest == "sha512":
+        verification = pbkdf2_sha512.verify(password, hash_pass)
+    else:
+        verification = pbkdf2_sha1.verify(password, hash_pass)
+    return verification
