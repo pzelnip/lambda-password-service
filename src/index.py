ce4b6db071175f85d8f53a8da87409c4b8959ed6
Adam Parkin <pzelnip@gmail.com>
2017-07-26 14:31:43 -0700

    Refactor lambda_helper for simplicity

--- src/index.py
+++ src/index.py
@@ -2,18 +2,19 @@ from __future__ import print_function
 
 from passlib.hash import pbkdf2_sha256, pbkdf2_sha512, pbkdf2_sha1, bcrypt
 
+HASH_MAPPINGS = {
+    "sha256": pbkdf2_sha256,
+    "sha512": pbkdf2_sha512,
+    "bcrypt": bcrypt,
+    "sha1": pbkdf2_sha1,
+}
+
+DEFAULT_HASH = pbkdf2_sha1
+
 
 def lambda_handler(event, context):
     digest = event['digest']
     hash_pass = event['hash_pass']
     password = event['password']
-
-    if digest == "sha256":
-        verification = pbkdf2_sha256.verify(password, hash_pass)
-    elif digest == "sha512":
-        verification = pbkdf2_sha512.verify(password, hash_pass)
-    elif digest == "bcrypt":
-        verification = bcrypt.verify(password, hash_pass)
-    else:
-        verification = pbkdf2_sha1.verify(password, hash_pass)
-    return verification
+    hash_fn = HASH_MAPPINGS.get(digest, DEFAULT_HASH)
+    return hash_fn.verify(password, hash_pass)
